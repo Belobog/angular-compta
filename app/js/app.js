39105352a13e5aefcc2b86dc77e651057d154f37
Boris MARIC <boris.maric777@gmail.com>
2015-05-18 00:54:46 +0200

    [Belobog]Ajout d une page home

--- app/js/app.js
+++ app/js/app.js
@@ -3,27 +3,32 @@
 /* App Module */
 
 var comptaApp = angular.module('comptaApp', [
-  'ngRoute',
-  'phonecatAnimations',
-  'phonecatControllers',
-  'saisirCourseController',
-  'phonecatFilters',
-  'phonecatServices'
+    'ngRoute',
+    'phonecatAnimations',
+    'phonecatControllers',
+    'saisirCourseController',
+    'homeController',
+    'phonecatFilters',
+    'phonecatServices'
 ]);
 
 comptaApp.config(['$routeProvider','$locationProvider',
-  function($routeProvider,$locationProvider) {
-    $routeProvider.
-      when('/saisir-course', {
-        templateUrl: 'views/saisir-course.html',
-        controller: 'SaisirCourseCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/saisir-course'
-      });
-      $locationProvider.html5Mode(true);
-  }]);
+    function($routeProvider,$locationProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            }).
+            when('/saisir-course', {
+                templateUrl: 'views/saisir-course.html',
+                controller: 'SaisirCourseCtrl'
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+        $locationProvider.html5Mode(true);
+    }]);
